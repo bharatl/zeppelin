7b6bbede65a1620da65e8a7de6659943ff0d7078
M BHARAT LAL <mbharatlal@ms-macbook-pro.in.ibm.com>
2016-03-01 16:25:40 +0530

    first commit

---
+++ zeppelin-zengine/src/main/java/org/apache/zeppelin/notebook/repo/DBNotebookRepo.java
@@ -0,0 +1,213 @@
+package org.apache.zeppelin.notebook.repo;
+
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import org.apache.zeppelin.conf.ZeppelinConfiguration;
+import org.apache.zeppelin.notebook.Note;
+import org.apache.zeppelin.notebook.NoteInfo;
+import org.apache.zeppelin.notebook.Paragraph;
+import org.apache.zeppelin.scheduler.Job;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+import java.sql.*;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Properties;
+
+/**
+ *
+ */
+
+public class DBNotebookRepo implements NotebookRepo {
+  Logger logger = LoggerFactory.getLogger(DBNotebookRepo.class);
+
+  private String DB_SCHEMA = "dash104750";
+  private String USERNAME = "dash104750";
+  private String PASSWORD = "HwK8U4gUlIrg";
+  private String DRIVER = "COM.ibm.db2os390.sqlj.jdbc.DB2SQLJDriver";
+  private String JDBCURL = "jdbc:db2://awh-yp-small02.services.dal.bluemix.net:50000/BLUDB";
+
+  private Connection con = null;
+
+  public DBNotebookRepo(ZeppelinConfiguration conf) {
+    try {
+      logger.info("Connecting to Database !!");
+      Class.forName(DRIVER);
+      Properties connectionProps = new Properties();
+      connectionProps.put("user", USERNAME);
+      connectionProps.put("password", PASSWORD);
+      con = DriverManager.getConnection(JDBCURL, connectionProps);
+      logger.info("Connected to Database !!" + con.toString());
+    }
+    catch (ClassNotFoundException e) {
+      e.printStackTrace();
+    }
+    catch (SQLException sqle)  {
+      logger.error("EXIT: SQLException getConnection, Message:" +
+              sqle.getMessage() + " - " + sqle.getErrorCode());
+    }
+  }
+
+  @Override
+  public List<NoteInfo> list() throws IOException {
+
+    logger.info("Entered list call");
+    List<NoteInfo> infos = new LinkedList<NoteInfo>();
+
+    String query = "SELECT NOTEBOOK_ID FROM " + DB_SCHEMA + ".NOTEBOOKCONTENT ";
+
+
+    try {
+      PreparedStatement ps1 = con.prepareStatement(query);
+      ResultSet result = ps1.executeQuery();
+
+      while (result.next()) {
+        Note note = getNote(result.getString("NOTEBOOK_ID"));
+        NoteInfo info = new NoteInfo(note);
+        if (info != null) {
+          infos.add(info);
+        }
+      }
+
+    } catch (SQLException sqle) {
+      logger.error("EXIT: SQLException getConnection, Message:" +
+              sqle.getMessage() + " - " + sqle.getErrorCode());
+      sqle.printStackTrace();
+    }
+
+    logger.info("left list call");
+    return infos;
+  }
+
+  @Override
+  public Note get(String noteId) throws IOException {
+
+    logger.info("get list call");
+    return getNote(noteId);
+
+  }
+
+  private Note getNote (String noteId) {
+
+    Note note = new Note();
+
+    if (con == null) {
+      logger.error("DB connection failed");
+      return null;
+    }
+    String query1 = "SELECT NOTEBOOK_ID, NOTEBOOK_DATA FROM " + DB_SCHEMA +
+            ".NOTEBOOKCONTENT WHERE NOTEBOOK_ID = ?";
+    try {
+      PreparedStatement ps1 = con.prepareStatement(query1);
+      ps1.setString(1, noteId);
+      Clob clob =  null;
+      ResultSet result = ps1.executeQuery();
+      if (result.next()) {
+        clob = result.getClob(2);
+      }
+      GsonBuilder gsonBuilder = new GsonBuilder();
+      gsonBuilder.setPrettyPrinting();
+      Gson gson = gsonBuilder.create();
+
+
+      if (clob != null) {
+        note = gson.fromJson(clob.getSubString(1, (int) clob.length()), Note.class);
+      }
+      for (Paragraph p : note.getParagraphs()) {
+        if (p.getStatus() == Job.Status.PENDING || p.getStatus() == Job.Status.RUNNING) {
+          p.setStatus(Job.Status.ABORT);
+        }
+      }
+
+    } catch (SQLException sqle) {
+      logger.error("EXIT: SQLException getConnection, Message:" +
+              sqle.getMessage() + " - " + sqle.getErrorCode());
+      sqle.printStackTrace();
+    }
+
+    return note;
+
+  }
+
+  @Override
+  public void save(Note note) throws IOException {
+    GsonBuilder gsonBuilder = new GsonBuilder();
+    gsonBuilder.setPrettyPrinting();
+    Gson gson = gsonBuilder.create();
+    String json = gson.toJson(note);
+    PreparedStatement ps;
+
+    String checkQuery = "SELECT NOTEBOOK_ID FROM " + DB_SCHEMA +
+            ".NOTEBOOKCONTENT WHERE NOTEBOOK_ID = ?";
+
+    String insertQuery = "INSERT INTO " + DB_SCHEMA + ".NOTEBOOKCONTENT  VALUES (?,?)";
+
+    String updateQuery = "UPDATE " + DB_SCHEMA +
+            ".NOTEBOOKCONTENT SET NOTEBOOK_DATA = ? WHERE NOTEBOOK_ID = ?";
+
+    try {
+      ps = con.prepareStatement(checkQuery);
+      ps.setString(1, note.getId());
+      ResultSet rset =  ps.executeQuery();
+      if (rset.next()) {
+        ps = con.prepareStatement(updateQuery);
+        ps.setString(1, json);
+        ps.setString(2, note.getId());
+        if (ps.executeUpdate() == 1) {
+          logger.info("update successfull");
+        }
+
+      } else {
+        ps = con.prepareStatement(insertQuery);
+        ps.setString(1, note.getId());
+        ps.setString(2, json);
+        if (ps.executeUpdate() == 1) {
+          logger.info("save successfull");
+        }
+      }
+
+    } catch (SQLException sqle) {
+      logger.error("EXIT: SQLException getConnection, Message:" +
+              sqle.getMessage() + " - " + sqle.getErrorCode());
+      sqle.printStackTrace();
+    }
+
+
+  }
+
+  @Override
+  public void remove(String noteId) throws IOException {
+
+    PreparedStatement ps;
+
+    String checkQuery = "DELETE FROM " + DB_SCHEMA +
+            ".NOTEBOOKCONTENT WHERE NOTEBOOK_ID = ?";
+    try {
+      ps = con.prepareStatement(checkQuery);
+      ps.setString(1, noteId);
+      if (ps.executeUpdate() == 1) {
+        logger.info("Notebook " + noteId + " deleted successfully");
+      }
+      
+    } catch (SQLException sqle) {
+      logger.error("EXIT: SQLException getConnection, Message:" +
+              sqle.getMessage() + " - " + sqle.getErrorCode());
+      sqle.printStackTrace();
+    }
+
+  }
+
+  @Override
+  public void close() {
+    //no-op
+  }
+
+  @Override
+  public void checkpoint(String noteId, String checkPointName) throws IOException {
+    // no-op
+    logger.info("Checkpoint feature isn't supported in {}", this.getClass().toString());
+  }
+}
